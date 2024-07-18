0a51a6f6d5364051e79a3d1504daa9989db2b81a
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-07-18 23:26:28 +0400

    snippets

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,11 +1,31 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((item) => {
-    return <div key={item.id}>{item.title}</div>;
+    return (
+      <Link
+        href={`/snippets/${item.id}`}
+        className={"flex justify-between items-center p-2 border rounded"}
+        key={item.id}
+      >
+        <div>{item.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className={"flex m-2 justify-between items-center"}>
+        <h1 className={"text-xl font-bold"}>Snippets</h1>
+        <Link href={"/snippets/new"} className={"border p-2 rounded"}>
+          NEW
+        </Link>
+      </div>
+      {renderedSnippets}
+    </div>
+  );
 }
--- snippets/src/app/snippets/[id]/not-found.tsx
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div>
-      <h1>Snippet not found</h1>
+      <h1 className={"text-xl bold"}>Snippet not found</h1>
     </div>
   );
 }
